d14d50c04ec8ab9142cb32f66506e0e6b59786b4
Ana Cristina Betan <anacristinabetan@MacBook-Pro-de-Ana.local>
2018-08-17 12:48:51 -0500

    first commit

---
+++ PruebaAbner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaAbner
+//
+//  Created by Ana Cristina Betan on 17/08/18.
+//  Copyright © 2018 Ana Cristina Betan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
